3989791287406315514b3537c9a575f65e28a0ab
eduardotecnologo <eduardotecnologo@hotmail.com>
2021-07-13 12:20:17 -0700

    img topo

--- alura-kut/pages/_app.js
+++ alura-kut/pages/_app.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import { AlurakutStyles } from '../src/lib/AlurakutCommons';
+import { AlurakutStyles } from '../src/lib/AlurakutCommons.js';
 
 const GlobalStyle = createGlobalStyle`
   /* Reset CSS (Necolas Reset CSS <3) */
