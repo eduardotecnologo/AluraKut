b7e135d4fc81bfd0c72f35093deca66bdcb77e38
eduardotecnologo <eduardotecnologo@hotmail.com>
2021-07-12 23:27:48 -0700

    AlurakutCommons

--- alura-kut/pages/index.js
+++ alura-kut/pages/index.js
@@ -1,6 +1,6 @@
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons.js';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSidebar(propriedades) {
